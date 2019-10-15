7a9304443306ba60a57615ea93c726efe0206c89
EEEeelijaHhHHh <elijahs.adjutovs@gmail.com>
2019-10-15 13:28:04 +0300

    Added vuex posts app

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,44 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <PostForm />
+    <form class="limit-form" @submit.prevent="submitHandler">
+      <input class="limit-input" type="text" placeholder="Enter limit of posts" v-model="limit">
+      <button class="limit-btn" type="submit">Change</button>
+    </form>
+    <h1>{{ postsCount }}</h1>
+    <div 
+      class="post"
+      v-for="post of validPosts"
+      :key="post.id"
+    >
+      <h2>{{ post.title }}</h2>
+      <p>{{ post.body }}</p>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import {mapGetters, mapActions} from 'vuex'
+import PostForm from './components/PostForm'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      limit: undefined
+    }
+  },
+  components: { PostForm },
+  computed: mapGetters(['validPosts', 'postsCount']),
+  methods: {
+    ...mapActions(['fetchPosts']),
+    submitHandler() {
+      this.fetchPosts(this.limit)
+      this.limit = ''
+    }
+  },
+  mounted() {
+    this.fetchPosts(this.limit)
   }
 }
 </script>
@@ -23,6 +50,32 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 5rem auto;
+  width: 500px;
+}
+
+.post {
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  padding: 1rem;
+  margin-bottom: 1rem;
+}
+
+.limit-form {
+  display: flex;
+  justify-content: center;
+  margin-top: 1rem;
+}
+
+.limit-input {
+  border: 1px solid #ccc;
+  border-radius: 2px;
+  padding: .5rem;
+  margin-bottom: 1rem;
+}
+
+.limit-btn {
+  width: 100px;
+  height: 33px;
 }
 </style>
---
+++ src/components/PostForm.vue
@@ -0,0 +1,42 @@
+<template>
+  <form @submit.prevent="submitHandler">
+    <input type="text" placeholder="Title" v-model="title">
+    <input type="text" placeholder="Body" v-model="body">
+    <button type="submit">Create Post</button>
+  </form>
+</template>
+
+<script>
+import { mapMutations } from 'vuex'
+
+export default {
+  data() {
+    return {
+      title: '',
+      body: ''
+    }
+  },
+  methods: {
+    ...mapMutations(['createPost']),
+    submitHandler() {
+      this.createPost({
+        title: this.title,
+        body: this.body,
+        id: Date.now()
+      })
+      this.title = this.body = ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+  input {
+    display: block;
+    width: 100%;
+    border: 1px solid #ccc;
+    border-radius: 2px;
+    padding: .5rem;
+    margin-bottom: 1rem;
+  }
+</style>
